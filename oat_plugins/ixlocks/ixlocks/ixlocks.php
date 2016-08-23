0faaa2cb6220b8cfc835f22e62edc8b7375ad552
Fernando Nunes <domusonline@gmail.com>
2016-08-23 23:46:30 +0200

    - Corrected a syntax issue with newer versions of PHP

--- oat_plugins/ixlocks/ixlocks/ixlocks.php
+++ oat_plugins/ixlocks/ixlocks/ixlocks.php
@@ -4,8 +4,8 @@
  * Copyright (c) 2016 Fernando Nunes - domusonline@gmail.com
  * License: This script is licensed as GPL V2 ( http://www.gnu.org/licenses/old-licenses/gpl-2.0.html )
  * $Author: Fernando Nunes - domusonline@gmail.com$
- * $Revision 2.0.1 $
- * $Date 2016-02-22 02:38:48$
+ * $Revision: 2.0.4 $
+ * $Date 2016-08-23 23:46:30$
  * Disclaimer: This software is provided AS IS, without any kind of guarantee. Use at your own risk.
  *             Although the author is/was an IBM employee, this software was created outside his job engagements.
  *             As such, all credits are due to the author.
@@ -118,7 +118,7 @@ class ixlocks {
 		*/
 
 		require_once ROOT_PATH."lib/gentab.php";
-		$tab = new gentab(&$this->idsadmin);
+		$tab = new gentab($this->idsadmin);
 
 
 		$qry =  "SELECT " .
@@ -212,7 +212,7 @@ class ixlocks {
 		*/
 
 		require_once ROOT_PATH."lib/gentab.php";
-		$tab = new gentab(&$this->idsadmin);
+		$tab = new gentab($this->idsadmin);
 
 
 		$qry =  "SELECT " .
@@ -293,7 +293,7 @@ class ixlocks {
 		*/
 
 		require_once ROOT_PATH."lib/gentab.php";
-		$tab = new gentab(&$this->idsadmin);
+		$tab = new gentab($this->idsadmin);
 
 		$qry =  "SELECT " .
 		"t.dbsname , " .
